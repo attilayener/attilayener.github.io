654cf6268c9e15941f6d6f36a2dfb7279794a72e
attilayener <atillayener1997@gmail.com>
2023-07-28 22:26:21 +0300

    Update styele.js

--- styele.js
+++ styele.js
@@ -3,7 +3,7 @@ document.addEventListener('keydown', function(event) {
     // Eğer 61 (klavyedeki nümerik tuşlardan 9'a basarsak)
     if (event.key === '9') {
         // Google'a yönlendir
-        window.location.href = 'https://www.google.com/';
+        window.location.href = 'https://www.youtube.com/watch?v=hUdWILxC8sY';
     }
   });
   
